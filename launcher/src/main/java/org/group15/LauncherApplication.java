9ad36151ac69bf36d35ccb514f7d9bed3bcce127
MAHMOUD DAHDOLAN <msdahdolan20@cit.just.edu.jo>
2025-03-08 16:55:34 +0300

    Test

---
+++ launcher/src/main/java/org/group15/LauncherApplication.java
@@ -0,0 +1,11 @@
+package org.group15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauncherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LauncherApplication.class, args);
+    }
+}
