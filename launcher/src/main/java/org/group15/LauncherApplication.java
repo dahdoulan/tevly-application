a92fdb55dc6b1fd4cdb20693dde53fef29dbaabb
MAHMOUD DAHDOLAN <msdahdolan20@cit.just.edu.jo>
2025-03-17 21:58:26 +0300

    added entity scan

--- launcher/src/main/java/org/group15/LauncherApplication.java
+++ launcher/src/main/java/org/group15/LauncherApplication.java
@@ -2,9 +2,11 @@ package org.group15;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"org.group15.tveely.*"})
+@EntityScan(basePackages = "org.group15.tveely.entities.*")
 @SpringBootApplication
 public class LauncherApplication {
     public static void main(String[] args) {
